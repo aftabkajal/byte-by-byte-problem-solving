cd2c4154ac2b0f8fa7e3cd675fd50b12ae364adc
Aftab Uddin Kajal <kajal.alpha@gmail.com>
2023-04-14 16:14:01 +0600

    FizFuzz problem solve

---
+++ src/FizzBuzz/ProblemSolve/FizzFuzzproblem.cs
@@ -0,0 +1,37 @@
+﻿//FizFuzz: Output numbers from 1 to x. if the number is divisible by 3, replace it with "Fizz". if it is divisible by 5, replace it with Buzz. 
+//if the number is divisible by 3 and 5 then replace with FizzBuzz.
+//https://youtu.be/c0OMPDLef08
+
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz.ProblemSolve
+{
+    public class FizzFuzzproblem
+    {
+        public static string FizzBuzz(int number)
+        {
+            var fizzBuzz = new StringBuilder();
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                { 
+                    fizzBuzz.AppendLine("FizzBuzz"); 
+                }
+                else if (i % 3 == 0 )
+                {
+                    fizzBuzz.AppendLine("Fizz");
+                }
+                else if ( i % 5 == 0)
+                {
+                    fizzBuzz.AppendLine("Buzz");
+                }
+
+                fizzBuzz.AppendLine(i.ToString());
+
+            }
+            Console.WriteLine(fizzBuzz.ToString());
+            return fizzBuzz.ToString();
+        }
+    }
+}
---
+++ src/FizzBuzz/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FizzBuzz.ProblemSolve
+{
+    class ProblemSolver
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzFuzzproblem.FizzBuzz(100));
+        }
+    }
+}
