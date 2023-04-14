f861c648931d76e14465bd35d23e898179317d4b
Aftab Uddin Kajal <kajal.alpha@gmail.com>
2023-04-14 18:33:33 +0600

    SinglyLinkList solved

---
+++ src/FizzBuzz/ProblemSolve/SinglyLinkList.cs
@@ -0,0 +1,40 @@
+﻿namespace FizzBuzz.ProblemSolve
+{
+    //    Given a singly linked list, write a function to find nth-to-last element of list.
+
+    //eg:
+    //list = 1-> 2-> 3-> 4-> 5
+    // C# program to find N'th node from end of linked list
+
+    public class SinglyLinkList
+    {
+        public class Node
+        {
+            public int value;
+            public Node next;
+        }
+
+        public Node? NthToLast(Node node, int n)
+        {
+            Node current = node;
+            Node follower = node;
+            for (int i = 0; i < n; i++)
+            {
+                if (current == null)
+                {
+                    return null;
+                }
+                current = current.next;
+            }
+            while (current != null)
+            {
+                current = current.next;
+                follower = follower.next;
+            }
+            return follower;
+        }
+    }
+
+
+}
+
--- src/FizzBuzz/Program.cs
+++ src/FizzBuzz/Program.cs
@@ -5,7 +5,15 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine(FizzFuzzproblem.FizzBuzz(100));
+            //Console.WriteLine(FizzFuzzproblem.FizzBuzz(100));
+            LinkedList llist = new LinkedList();
+            llist.push(20);
+            llist.push(4);
+            llist.push(15);
+            llist.push(35);
+
+            // Function call
+            llist.PrintNthFromLast(4);
         }
     }
 }
