703e07d7aef4ea99722b79fca448539535f8e652
Aftab Uddin Kajal <kajal.alpha@gmail.com>
2023-04-14 18:34:18 +0600

    Error resolved

--- src/FizzBuzz/Program.cs
+++ src/FizzBuzz/Program.cs
@@ -6,14 +6,14 @@
         static void Main(string[] args)
         {
             //Console.WriteLine(FizzFuzzproblem.FizzBuzz(100));
-            LinkedList llist = new LinkedList();
-            llist.push(20);
-            llist.push(4);
-            llist.push(15);
-            llist.push(35);
+            //LinkedList llist = new LinkedList();
+            //llist.push(20);
+            //llist.push(4);
+            //llist.push(15);
+            //llist.push(35);
 
-            // Function call
-            llist.PrintNthFromLast(4);
+            //// Function call
+            //llist.PrintNthFromLast(4);
         }
     }
 }
